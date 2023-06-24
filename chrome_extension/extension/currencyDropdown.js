daf3fef012c7492c0b059a718f4bfea62fae644e
ofekzil <ofekzil162@gmail.com>
2023-06-24 13:14:44 -0700

    added default currency

--- chrome_extension/extension/currencyDropdown.js
+++ chrome_extension/extension/currencyDropdown.js
@@ -1,4 +1,8 @@
 const currencies = Intl.supportedValuesOf("currency");
 for (var currency of currencies) {
-    document.write("<option>" + currency + "</option>");
+    if (currency === "USD") {
+        document.write("<option selected='selected'>" + currency + "</option>");
+    } else { 
+        document.write("<option>" + currency + "</option>");
+    }
 }
